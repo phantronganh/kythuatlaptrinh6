30b1d3c5e3f839d19a684ad2378fa7a636bd1888
phantronganh <62613290+phantronganh@users.noreply.github.com>
2020-05-25 10:23:25 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,31 @@
+class class_py_solution:
+    def roman_to_int(self,s):
+        self.s=s
+    rom_val={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    int_val=0
+s=int(input(" Nhập số la mã: "))
+for s in range():
+    if s=='I':
+        s=1;
+    else:
+        if s=='V':
+            s=5;
+    else:
+        if s=='X':
+            s=10;
+    else:
+        if s=='L':
+            s=50;
+    else:
+        if s=='C':
+            s=100;
+    else:
+        if s=='D':
+            s=500;
+    else:
+        if s=='M':
+            s=1000;
+    else:
+        print(" xin hãy nhập lại dãy số la mã  :")
+LM=class_py_solution(s)
+print(LM.roman_to_int())
