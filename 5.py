09ee569ee66e680dbaf3f2906e42a5aec140325b
phantronganh <62613290+phantronganh@users.noreply.github.com>
2020-05-25 10:24:25 +0700

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,8 @@
+class chuỗi(object):
+    def chuoi(self,c):
+        self.c=c
+    def reverse_words:
+        return join(reverse(c.split))
+a=int(input("nhập chuỗi :")
+d=chuỗi(c)
+print(d.reverse_words([::-1]))
